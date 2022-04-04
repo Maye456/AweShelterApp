2d606f1c88ec422d8e14995ff738718324d55312
JeannaMaye <54680474+Maye456@users.noreply.github.com>
2022-04-03 21:48:00 -0700

    Logging AnimalService

--- src/main/java/com/gcu/data/AnimalDataService.java
+++ src/main/java/com/gcu/data/AnimalDataService.java
@@ -4,10 +4,11 @@ import java.util.List;
 
 import javax.sql.DataSource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
-
 import com.gcu.model.AnimalEntity;
 
 @Service
@@ -16,6 +17,8 @@ public class AnimalDataService implements IAnimalDataAccess<AnimalEntity>
 	@Autowired
 	private AnimalRepository animalRepository;
 	@SuppressWarnings("unused")
+	
+	Logger logger = LoggerFactory.getLogger(AnimalDataService.class);
 	
 	private DataSource dataSource;
 	private JdbcTemplate jdbcTemplateObject;
@@ -30,40 +33,75 @@ public class AnimalDataService implements IAnimalDataAccess<AnimalEntity>
 
 	@Override
 	public AnimalEntity getById(int Id) {
+		// Log the API Call
+        logger.info("Entering AnimalDataService.getById()");
+        // Log the API Call
+        logger.info("Exiting AnimalDataService.getById()");
+        
 		return animalRepository.findById((long) Id).orElse(null);
 	}
 
 	@Override
 	public List<AnimalEntity> getAnimals() {
+		// Log the API Call
+        logger.info("Entering AnimalDataService.getAnimals()");
+        
 		List<AnimalEntity> animals = (List<AnimalEntity>) animalRepository.findAll();
+		
+		// Log the API Call
+        logger.info("Exiting AnimalDataService.getAnimals()");
 		return animals;
 	}
 
 	@Override
 	public List<AnimalEntity> searchAnimal(String searchTerm) {
+		// Log the API Call
+        logger.info("Entering AnimalDataService.searchAnimal()");
+        
 		List<AnimalEntity> result = animalRepository.findByNameContainingIgnoreCase(searchTerm);
+		
+		// Log the API Call
+        logger.info("Exiting AnimalDataService.searchAnimal()");
 		return result;
 	}
 
 	@Override
 	public int addOne(AnimalEntity newAnimal) {
+		// Log the API Call
+        logger.info("Entering AnimalDataService.addOne()");
+        
 		AnimalEntity result = animalRepository.save(newAnimal);
 		if (result == null)
         {
             return 0;
         }
+		
+		// Log the API Call
+        logger.info("Exiting AnimalDataService.addOne()");
         return (int) result.getId();
 	}
 
 	@Override
 	public boolean deleteOne(long id) {
+		// Log the API Call
+        logger.info("Entering AnimalDataService.deleteOne()");
+        
 		animalRepository.deleteById(id);
+		
+		// Log the API Call
+        logger.info("Exiting AnimalDataService.deleteOne()");
 		return true;
 	}
 
 	@Override
 	public AnimalEntity updateOne(long idToUpdate, AnimalEntity updateAnimal) {
+		// Log the API Call
+        logger.info("Entering AnimalDataService.updateOne()");
+        
 		AnimalEntity result = animalRepository.save(updateAnimal);
+		
+		// Log the API Call
+        logger.info("Exiting AnimalDataService.updateOne()");
 		return result;
 	}
 }
