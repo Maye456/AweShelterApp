8368d5191134f6655117153f65aabb54d90c49eb
Sena <70973589+jessenaqu@users.noreply.github.com>
2022-04-15 17:21:30 -0700

    javadoc
    interfaces update

--- src/main/java/com/gcu/business/IAnimalBusinessService.java
+++ src/main/java/com/gcu/business/IAnimalBusinessService.java
@@ -6,10 +6,33 @@ import com.gcu.model.AnimalModel;
 
 public interface IAnimalBusinessService 
 {
+	/**
+	 * Get the animal by id
+	 */
 	public AnimalModel getById(int Id);
+	
+	/**
+	 * Get all the animals 
+	 */
 	public List<AnimalModel> getAnimals();
+	
+	/**
+	 * Get all the animals from what is searched
+	 */
 	public List<AnimalModel> searchAnimals(String searchTerm);
+
+	/**
+	 * This will add one animal 
+	 */
 	public int addOne(AnimalModel newAnimal);
+	
+	/**
+	 * This will delete one animal 
+	 */
 	public boolean deleteOne(long id);
+	
+	/**
+	 * Update an animal
+	 */
 	public AnimalModel updateOne(long idToUpdate, AnimalModel updateAnimal);
 }
